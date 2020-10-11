a8348fe4a5748ce9f5a122fbbf28bfc79b61f82b
FireYourGuns <chazovby@gmail.com>
2020-10-11 19:01:43 +0300

    fix: dream team 1

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+  if (arr === undefined || arr === []) {return false}   //
+  var name="";
+  var str = "";
+    for (var i = 0; i < arr.length; i++) {
+      var Y = "";
+      Y += arr[i]; 
+        str += Y.split("").splice(0,1);
+    }
+  var X = str.split("").sort();
+  if (X === undefined || X === []) {return false}   //
+  for (i = 0; i < X.length; i++) {
+    var Z = X[i];
+    if (X[i] === X[i].toUpperCase() && isNaN(Z)) {
+      name += X[i];
+    }
+  }
+
+return name;
 };

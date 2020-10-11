fdebdb97e2b1d70b3f024bdabbc470b956385233
FireYourGuns <chazovby@gmail.com>
2020-10-11 16:35:22 +0300

    feat: implement part of transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new RangeError('Error');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  var new_Array = [];
+    var X = arr.length;
+        for (var i = 0; i < X; i++) {
+          new_Array.push(arr[i]);
+            if (arr[i] === '--double-next') {
+              new_Array[i] = arr[i+1];
+            }
+            if (arr[i] === '--discard-prev') {
+              new_Array.pop();
+              new_Array.pop();
+            }
+            if (arr[i] === '--discard-next') {
+              new_Array.pop();
+              i+= 1;
+            }
+            if (arr[i] === '--double-prev') {
+              new_Array[i] = arr[i-1];
+            }
+        }
+return new_Array;
 };

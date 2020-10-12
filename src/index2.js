7fb5eb7b23f059e36689e0b11f792114b076c783
FireYourGuns <chazovby@gmail.com>
2020-10-12 10:54:31 +0300

    fix: transform array3

---
+++ src/index2.js
@@ -0,0 +1,37 @@
+function transform(arr) {
+  if (typeof arr !== 'array') {
+    throw new Error('Введено не верное значение!')
+  }
+  var new_Array = [];
+  var X = arr.length;
+      for (var i = 0; i < X; i++) {
+        new_Array.push(arr[i]);
+          if (arr[i] === '--double-prev') {
+                      if (arr[i-1] !== undefined) {
+                          if (arr[i-2] !=='--discard-next') {
+                                          new_Array.push(new_Array[new_Array.length-1]);
+                          }
+                      }
+                      new_Array.pop();
+          }
+          if (arr[i] === '--double-next') {
+            arr[i+1] == undefined ? new_Array.pop() : new_Array[i] = arr[i+1]
+          } 
+          if (arr[i] === '--discard-prev') {
+            new_Array.pop();
+            new_Array.pop();
+          }
+          if (arr[i] === '--discard-next') {
+            new_Array.pop();
+          }
+           if (arr[i-1] === '--discard-next') {
+            new_Array.pop();
+          }
+          
+         
+      }
+return new_Array;
+};
+
+
+console.log(    transform({'foo': 'bar'})      )
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,16 @@
 const CustomError = require("../extensions/custom-error");
-
+var Depth = 1;
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+      for (var i = 0; i < arr.length; i++) {
+          if (arr[i].constructor === Array) {
+            Depth += 1;
+            calculateDepth(arr[i]);
+          } else {
+            return Depth;
+            }
+          
+      }
+      
   }
 };

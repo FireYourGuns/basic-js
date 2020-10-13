025640d17dd9ecf6810ab03239228a5c02eb9895
FireYourGuns <chazovby@gmail.com>
2020-10-13 10:25:01 +0300

    feat: implement all functions of repeater except the last

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,15 +2,62 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(str, options) {
   var answer = str;
+  var aRT = "";
+ 
+  if (!options.separator && !options.addition && !options.additionsRepeatTimes) {
+    for (var i = 1; i < options.repeatTimes; i++)  {
+      answer += "+" + str
+    }
+      
+    return answer
+  }
+
+  if (!options.addition && !options.additionsRepeatTimes) {
+    for (var i = 1; i < options.repeatTimes; i++)  {
+      answer += options.separator + str
+    }
+      
+    return answer
+  }
+
+
+
+  //вычисляем "aRT" ---------
+  if (options.addition && options.additionSeparator) {
+    for (var l = 0; l < options.additionRepeatTimes-1; l++) {
+      aRT += options.addition + options.additionSeparator
+    }
+                                      //aRT += options.addition; // к "aRT" добавляем "а"
+  }  
+  // ------------
+
+ // вычисление строки
+  if (options.repeatTimes > 0 || options.repeatTimes==undefined) {
+      for (let i = 1; i < options.repeatTimes; i++ ) {
+          answer += aRT + options.addition + options.separator + str;
+      }
+      answer += aRT + options.addition
+  }
+  // ----------
+
+    return answer;
+};
+
+/*
+  var answer = str;
   if (options.repeatTimes > 1) {
       for (let i = 1; i < options.repeatTimes; i++ ) {
+        if (options.addition && options.additionRepeatTimes) {
+            answer += options.addition + options.separator + str;
+          } else
           if (options.separator) {
             answer += options.separator + str ;
           } else {
           answer += "+" + str;
           }
-          
       }
   }
-    return answer;
-};
+  if (options.addition) {
+        answer += options.addition
+      }
+    return answer;*/

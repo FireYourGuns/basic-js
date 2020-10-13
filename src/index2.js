ae11a38dfdccafbcb9d6e0d181bbfd77403b78aa
FireYourGuns <chazovby@gmail.com>
2020-10-13 20:57:02 +0300

    feat: implement 5/8 vigenere-cipher

--- src/index2.js
+++ src/index2.js
@@ -1,52 +1,46 @@
-function repeater(str, options) {
-  var answer = str;
-  var aRT = "";
-  if (options.addition==false) {
-    options.addition = 'false'
-  }
-  if (options.addition==null) {
-    options.addition = 'null'
-  }
- 
-  if (!options.separator && !options.addition && !options.additionsRepeatTimes) {
-    for (var i = 1; i < options.repeatTimes; i++)  {
-      answer += "+" + str
+class VigenereCipheringMachine {
+    constructor (dir) {
+        this.direction = dir;
+        this.alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+                                                                       
     }
-      
-    return answer
-  }
+    
+          encrypt(message, key) {
+            if (message==undefined || key==undefined) {
+                throw new Error('Что-то пошло не так ((');
+            }
+            var m = message.toLowerCase().split('');
+            var k = key.toLowerCase().split('');
+            var string = "";
+            var j = -1;
+            for (var i = 0; i < m.length; i++) {
+              if (this.alphabet.indexOf(m[i]) !== -1) {
+                j++
+              } 
+              if (j >= k.length) {
+                j %= k.length;
+              }
+              if (this.alphabet.indexOf(m[i]) === -1) {
+                string += m[i];
+              } else {
+              var index = this.alphabet.indexOf(m[i]) + this.alphabet.indexOf(k[j]);
+              if (index >= this.alphabet.length) {
+                index = index % this.alphabet.length;
+              }
+              string += this.alphabet[index];
+              }
+            }
+            return string.toUpperCase()
+            
+          }    
 
-  if (!options.addition && !options.additionsRepeatTimes) {
-    for (var i = 1; i < options.repeatTimes; i++)  {
-      answer += options.separator + str
-    }
-      
-    return answer
-  }
-
-
-
-  //вычисляем "aRT" ---------
-  if (options.addition && options.additionSeparator) {
-    for (var l = 0; l < options.additionRepeatTimes-1; l++) {
-      aRT += options.addition + options.additionSeparator
-    }
-                                      //aRT += options.addition; // к "aRT" добавляем "а"
-  }  
-  // ------------
-
- // вычисление строки
-  if (options.repeatTimes > 0 || options.repeatTimes==undefined) {
-      for (let i = 1; i < options.repeatTimes; i++ ) {
-          answer += aRT + options.addition + options.separator + str;
-      }
-      answer += aRT + options.addition
-  }
-  // ----------
-
-    return answer;
+          decrypt(message, key) {
+            if (message==undefined || key==undefined) {
+              throw new Error('Что-то пошло не так ((');
+          }
+          }
 };
 
-console.log(         repeater    (true, { repeatTimes: 3, separator: '??? ', addition: false, additionRepeatTimes: 2, additionSeparator: '!!!' })     )
+console.log(         new VigenereCipheringMachine(false).encrypt('attack at dawn!', 'alphonse')  )
          
-         'TESTstrADD!'
+       
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,46 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor (dir) {
+      this.direction = dir;
+      this.alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+                                                                     
   }
+  
+        encrypt(message, key) {
+          if (message==undefined || key==undefined) {
+              throw new Error('Что-то пошло не так ((');
+          }
+          var m = message.toLowerCase().split('');
+          var k = key.toLowerCase().split('');
+          var string = "";
+          var j = -1;
+          for (var i = 0; i < m.length; i++) {
+            if (this.alphabet.indexOf(m[i]) !== -1) {
+              j++
+            } 
+            if (j >= k.length) {
+              j %= k.length;
+            }
+            if (this.alphabet.indexOf(m[i]) === -1) {
+              string += m[i];
+            } else {
+            var index = this.alphabet.indexOf(m[i]) + this.alphabet.indexOf(k[j]);
+            if (index >= this.alphabet.length) {
+              index = index % this.alphabet.length;
+            }
+            string += this.alphabet[index];
+            }
+          }
+          return string.toUpperCase()
+          
+        }    
+
+        decrypt(message, key) {
+          if (message==undefined || key==undefined) {
+            throw new Error('Что-то пошло не так ((');
+        }
+        }
 }
 
 module.exports = VigenereCipheringMachine;
